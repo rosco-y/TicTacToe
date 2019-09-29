7b1eae26c6e592c9fe6f4d3bb11ec144e157b564
Ross Ylitalo <ross.ylitalo@gmail.com>
2019-09-28 21:13:33 -0400

    simple scene with two cubes and a sphere.  I'm trying to create a scenerio in which one of the cubes will both react to a Trigger event (When it is "hit" with the sphere). and a MouseDown event. While the other cube will only react to a MouseDown event.

---
+++ TicTacToe/Assets/Scripts/cSphereTriggert.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cSphereTriggert : MonoBehaviour
+{
+        int _evenCount = 0;
+
+        private void OnTriggerEnter(Collider other)
+        {
+                if (other.tag == "TriggerCell")
+                        _evenCount++;
+        }
+
+        private void OnMouseDown()
+        {
+                _evenCount++;
+        }
+}
---
+++ TicTacToe/Assets/Scripts/cTriggerCell.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cTriggerCell : MonoBehaviour
+{
+        int _eventCount = 0;
+
+        private void OnTriggerEnter(Collider other)
+        {
+                if (other.tag == "TriggerCell")
+                        _eventCount++;
+        }
+
+        private void OnMouseDown()
+        {
+                _eventCount++;
+        }
+
+}
