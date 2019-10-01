ca8f0d54c2fc8a68ee36313f370f04134cc5c315
Ross Ylitalo <ross.ylitalo@gmail.com>
2019-10-01 07:32:21 -0400

    When a cell is clicked anywhere in the SuperGrid, it is identified by the subgrid it is in, and by it's position in the subgrid.
    i.e., the bottom left cell,row is (0,0),(0,1),(0,2)
    and the upper right cell,row is (8,6), (8,7) and (8,8)

--- TicTacToe/Assets/Scripts/cCell.cs
+++ TicTacToe/Assets/Scripts/cCell.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class cCell : MonoBehaviour
 {
-        int _eventCount = 0;
+        int _gridNo;
+        int _cellNo;
 
         private void OnMouseDown()
         {
-                _eventCount++;
-                print("Mouse Event");
+                //string parent = this.GetComponentInParent<c3x3>().name;
+                _gridNo = int.Parse(this.GetComponentInParent<c3x3>().name.Trim().Substring(1));
+                _cellNo = int.Parse(this.name.Trim().Substring(1));
+                print($"({_gridNo},{_cellNo})");
         }
 }
